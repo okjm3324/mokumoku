3aa2cc84c518468bfbdae3cfdd21f371b66034ef
okjm3324 <okjm.yuma@gmail.com>
2023-04-27 02:57:50 +0900

    Add: マイページにhobby,plofileの入力フォームを追加
    マイページ画面にhobby,plofileフォームを追加し、userに格納できるようにしました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    params.require(:user).permit(:email, :name, :password, :password_confirmation )
   end
 end
